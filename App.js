cd537207afe57f1156b693708bffd2b48be344e8
titouan00 <titouan.brunet@epitech.digital>
2023-09-25 09:49:46 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <MapView style={styles.map} />
+      <Text>Salut</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
